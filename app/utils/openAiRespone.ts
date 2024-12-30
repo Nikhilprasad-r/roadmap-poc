005fe2357d68e41af6c17a6648aa0264649b1257
Nikhil Prasad <nikhilprasadinbox@gmail.com>
2024-12-30 12:06:20 +0530

    initialized

---
+++ app/utils/openAiRespone.ts
@@ -0,0 +1,27 @@
+import OpenAI from "openai";
+import { zodResponseFormat } from "openai/helpers/zod";
+
+const openAiResponse = async (
+  systemPrompt: string,
+  userPrompt: string,
+  schema: any
+) => {
+  const openai = new OpenAI();
+  const completion = await openai.beta.chat.completions.parse({
+    model: "gpt-4o-mini",
+    messages: [
+      {
+        role: "system",
+        content: systemPrompt,
+      },
+      {
+        role: "user",
+        content: userPrompt,
+      },
+    ],
+
+    response_format: zodResponseFormat(schema, "event"),
+  });
+  return completion.choices[0].message.parsed;
+};
+export default openAiResponse;
